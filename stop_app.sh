89696fe893b21840b7ea87c3cfcc36cb679f34d4
Clément Ruiz <clement.ruiz@ynov.com>
2017-05-08 01:45:21 +0200

    Cleaning up

--- stop_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This turn off the entire application, removing every container attached to it.
-
-
-# In Order to Work, this script will use of the ./system/${INSTANCE_ID}/source_ct_list
-# From every line it'll find, run the docker command "docker container rm -f " for each.
-# Finally, --> docker-compose down
-
-declare INSTANCE_ID="1"                                       # This one will be dynamically handled later. Notify the quotes
