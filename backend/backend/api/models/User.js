de23acf5765a0292a01ceec3ffa394a41ee52d8b
Clément Ruiz <clement.ruiz@ynov.com>
2017-05-09 15:20:03 +0200

    Adding properties to User model

--- backend/backend/api/models/User.js
+++ backend/backend/api/models/User.js
@@ -8,4 +8,34 @@ _.merge(exports, {
 
   // Extend with custom logic here by adding additional fields, methods, etc.
 
+  attributes: {
+    active :{
+      type: 'boolean',
+      defaultsTo: false
+    },
+    teamID: {
+      model: 'team'
+    },
+    firstName: {
+      type: 'string',
+      required: true,
+      columnName: 'first_name'
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+      columnName: 'last_name'
+    },
+    description: {
+      type: 'string'
+    },
+    activationDate: {
+      type: 'datetime',
+      columnName: 'activated_at'
+    },
+    lastConnection: {
+      type: 'datetime',
+      columnName: 'last_con'
+    }
+  }
 });
