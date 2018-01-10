9f8b8229f74a048b90b9100ad2f05bf1d85bb164
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-01-10 17:17:39 +0100

    Adding "Create Container" functionality
    Created a source model + function which return next track.
    
    Need to be tested

--- app/app/app.py
+++ app/app/app.py
@@ -1,16 +1,23 @@
 from flask import Flask
 import docker
 
+import SourceModels
+
 app = Flask(__name__)
+docker_client = docker.DockerClient(
+    base_url='unix:///var/run/docker.sock',
+    version='auto'
+)
 
 @app.route('/')
 def hello_world():
-   return 'Welcome to Radio Bretzel'
+    return 'Welcome to Radio Bretzel'
 
 @app.route('/new')
-def create_source():
+SourceModels.create()
 
-
+@app.route('/next')
+SourceModels.select_next_track()
 
 if __name__ == '__main__':
-   app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
