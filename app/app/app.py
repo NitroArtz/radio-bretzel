1ba3d6a8dc912238d74d10523ee8458ee16eba7d
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-01-10 15:50:08 +0100

    /1.1-Generate-audio-source:

--- app/app/app.py
+++ app/app/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from api import app
+import docker
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
    return 'Welcome to Radio Bretzel'
+
+@app.route('/new')
+def create_source():
+
+
 
 if __name__ == '__main__':
    app.run(debug=True,host='0.0.0.0')
