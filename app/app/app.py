0973222d4230c1333ed7794f8c4568699274286e
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-01-10 14:40:13 +0100

    Preparing first functionnality
    Creating test and app directories, adding some requirements for dev
    (unittest)

--- app/app/app.py
+++ app/app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from api import app
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to Radio Bretzel'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
