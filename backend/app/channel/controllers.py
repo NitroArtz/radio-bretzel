cef3fba219fcca2d027ecc69cc8bbf480e1a950f
Lothaire Prince <lothaire.prince@y-nov.com>
2018-02-07 10:56:23 +0100

    random song

--- backend/app/channel/controllers.py
+++ backend/app/channel/controllers.py
@@ -6,6 +6,7 @@ from app.channel.models import Channel
 
 from ..channel import channel
 
+@channel.route('/next')
 def select_next_track():
    random_song = randint(1, 3)
    return "/audio/test%s.mp3" % random_song
@@ -24,6 +25,7 @@ def validate(**data):
 
 @channel.route('/', methods=['POST'])
 @channel.route('/<_id>', methods=['POST'])
+
 def create_channel():
    _id = request.values.get('_id')
    if not _id:
