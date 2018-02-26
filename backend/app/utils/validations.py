36c68172ac1632a81f2af1b1fe9b896526ed2d4b
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-02-26 01:27:03 +0100

    Spliting up utils.py in a package

--- backend/app/utils/validations.py
+++ backend/app/utils/validations.py
@@ -1,6 +1,6 @@
 import re
 
-def validate_slug(slug):
+def slug(slug):
    if len(slug) > 64:
       raise ValueError('slug is too long (max 64 characters)')
    elif not re.match('((?:[a-z][a-z]*[0-9]*[a-z0-9]*))(-?)((?:[a-z][a-z]*[0-9]*[a-z0-9]*)?)(-?)((?:[a-z][a-z]*[0-9]*[a-z0-9]*)?)', slug):
