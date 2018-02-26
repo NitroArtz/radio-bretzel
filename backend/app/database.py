cfa7a2b627c416f5bcc3f0abbed4e0a36d4c7a1c
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-02-26 01:29:24 +0100

    Reviewing Document class and database + implementing errors

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,11 @@
 from flask import current_app as app
-
 from flask_pymongo import PyMongo
+
+from app.errors import DatabaseError
+
+models = [
+   'channel',
+]
 
 def connect_db(app):
    """ Open a Mongodb connection."""
@@ -9,32 +14,50 @@ def connect_db(app):
 
 def init_db(app):
    """ Initiate DB connection at app startup """
-   if not hasattr(app, 'database'):
+   if not hasattr(app, 'mongo'):
       app.mongo = connect_db(app)
    return app
+
+def get_collection(name):
+   try:
+      if not hasattr(app, 'mongo'):
+         raise DatabaseError("Database unavailable")
+      if name in models:
+         collection = app.mongo.db[name]
+         return collection
+      else:
+         raise DatabaseError('Database Error - Unreferenced model')
+   except:
+      raise DatabaseError("Couldn't get collection object")
 
 class Document(object):
    """ Abstract class whose different models will inherit """
 
-   def save(collection, document):
-      """ Update or create provided document from given collection """
-      if not document.get('_id'):
-         return False
+   def save(self):
+      """ Update or create model's document in database """
       try:
-         collection.find_one({'_id': document['_id']})
+         collection = get_collection(self.model)
+         document = self.document()
+         existing_document = collection.find_one({'_id': document['_id']})
+         if existing_document:
+            collection.replace_one(existing_document, document)
+         else:
+            collection.insert_one(document)
+         return True
       except:
-         collection.insert_one(document)
-      else:
-         collection.replace_one({'_id': document['_id']}, document)
-      return True
+         raise SystemError("Couldn't save " + self.model + " in database")
 
-   def delete(collection, document):
+   def delete(self):
       """ Delete the current document from given collection """
-      if not document.get('_id'):
-         return False
       try:
-         collection.find_one({'_id': document['_id']})
+         collection = get_collection(self.model)
+         document = self.document()
+         if not document.get('_id'):
+            raise KeyError('No primary key given for object')
+         existing_document = collection.find_one({'_id': document['_id']})
+         if not existing_document:
+            raise DatabaseError('Document not found')
+         collection.remove(existing_document)
+         return True
       except:
-         return False
-      collection.remove({'_id': document['_id']})
-      return True
+         raise DatabaseError("Couldn't delete " + self.model + " in database")
