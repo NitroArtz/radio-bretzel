18cc666369767dc3c96ef14432f62dc013c1f1e0
Clement-Ruiz <clement.ruiz@ynov.Com>
2018-05-10 20:03:38 +0200

    adding new formats function popkeys()

--- backend/rb_backend/utils/formats.py
+++ backend/rb_backend/utils/formats.py
@@ -1,6 +1,13 @@
 def id_to_name(_id):
    return _id.replace('-', ' ').title()
 
+def pop_keys(data, *args):
+   poped_data = {}
+   for key in args:
+      value = data.pop(key, False)
+      if value : poped_data[key] = value
+   return poped_data
+
 def get_prefixed_keys(dictionnary, prefix, lowercase=True, trim=True):
    """
    This function returns key/value pairs which the key starts with given prefix.
@@ -15,9 +22,11 @@ def get_prefixed_keys(dictionnary, prefix, lowercase=True, trim=True):
       trim                    <bool>   :  if set to True (default), all matching
                                           keys name will have the given prefix trimed
    """
-   rv = {}
+   matching = {}
+   non_matching = {}
    for k, v in dictionnary.items():
       if not k.startswith(prefix):
+         non_matching[k] = v
          continue
       if trim:
          key = k[len(prefix):]
@@ -25,5 +34,5 @@ def get_prefixed_keys(dictionnary, prefix, lowercase=True, trim=True):
          key = k
       if lowercase:
          key = key.lower()
-      rv[key] = v
-   return rv
+      matching[key] = v
+   return {'matching': matching, 'others': non_matching}
